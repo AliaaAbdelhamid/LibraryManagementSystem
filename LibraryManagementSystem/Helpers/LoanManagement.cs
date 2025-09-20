3b9da838b294fcabd77550a6512733221fc106f1
Aliaa Tarek <Aliaa Tarek@LAPTOP-LJC895S6>
2025-09-20 15:35:23 +0300

    CRUD Operations

---
+++ LibraryManagementSystem/Helpers/LoanManagement.cs
@@ -0,0 +1,84 @@
+﻿using LibraryManagementSystem.Contexts;
+using LibraryManagementSystem.Entities;
+using LibraryManagementSystem.Entities.Enums;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Helpers
+{
+	internal static class LoanManagement
+	{
+		public static bool BorrowingBook(int MemberId, int BookId, int BorrowDays, LibraryDbContext dbContext)
+		{
+			try
+			{
+				var Member = dbContext.Members.Find(MemberId);
+				if (Member is null || Member.Status == MemberStatus.Suspended) return false;
+
+				var Book = dbContext.Books.Find(BookId);
+				if (Book is null || Book.AvailableCopies == 0) return false;
+				var Loan = new Loan();
+
+				dbContext.Loans.Add(Loan);
+				var memberLoan = new MemberLoans()
+				{
+					Loan = Loan, MemberId = MemberId, BookId = BookId,
+					DueDate = DateTime.Now.AddDays(BorrowDays)
+				};
+				dbContext.MemberLoans.Add(memberLoan);
+				Book.AvailableCopies -= 1;
+				dbContext.SaveChanges();
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+
+		}
+
+		public static bool ReturnBook(int memberId, int bookId, LibraryDbContext dbContext)
+		{
+			try
+			{
+				var memberLoan = dbContext.MemberLoans
+										  .Include(ml => ml.Loan)
+										  .Include(ml => ml.Book)
+										  .Include(ml => ml.Member)
+										  .FirstOrDefault(ml => ml.MemberId == memberId && ml.BookId == bookId && ml.ReturnDate == null);
+				if (memberLoan is null) return false;
+				
+				var Book = memberLoan.Book;
+				var Member = memberLoan.Member;
+				var Loan = memberLoan.Loan;
+				memberLoan.ReturnDate = DateTime.Now;
+				Book.AvailableCopies += 1;
+
+				if (memberLoan.ReturnDate.Value.Date > memberLoan.DueDate.Date)
+				{
+					var overdueDays = (memberLoan.ReturnDate.Value - memberLoan.DueDate).Days;
+					decimal dailyFine = 0.1M * Book.Price;
+					var Fine = new Fine() { Amount = overdueDays * dailyFine, Loan = Loan };
+					dbContext.Fines.Add(Fine);
+					Member.Status = MemberStatus.Suspended;
+					Loan.Status = LoanStatus.Overdue;
+				}
+				else
+				{
+					Loan.Status = LoanStatus.Returned;
+				}
+
+				dbContext.SaveChanges();
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+	}
+}
--- LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/Program.cs
@@ -1,4 +1,8 @@
 ﻿using LibraryManagementSystem.Contexts;
+using LibraryManagementSystem.Entities;
+using LibraryManagementSystem.Helpers;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 namespace LibraryManagementSystem
 {
@@ -7,9 +11,104 @@ namespace LibraryManagementSystem
 		static void Main(string[] args)
 		{
 			using LibraryDbContext dbContext = new LibraryDbContext();
+			LibraryDbContextSeed.SeedData(dbContext);
 
-			bool Result = LibraryDbContextSeed.SeedData(dbContext);
-			Console.WriteLine(Result ? "Data Seeded Successfully" : "Seeding Failed");
+			#region CRUD Operations 
+
+			#region Retrieve the book title, its category title , and the author’s full name for all books whose price is greater than 300
+
+			//var Books = dbContext.Books.Where(B => B.Price > 300).Select(B => new
+			//{
+			//	BookTitle = B.Title,
+			//	Category = B.BookCategory.Title,
+			//	Author = (B.BookAuthor.FirstName ?? "") + " " + (B.BookAuthor.LastName ?? "")
+			//}).ToList();
+
+
+			//foreach (var item in Books)
+			//	Console.WriteLine(item);
+
+
+			#endregion
+
+			#region Retrieve All Authors And His/Her Books if Exists. 
+
+			//var Authors = dbContext.Authors.Include(X => X.AuthorBooks).ToList();
+
+			//foreach (var author in Authors)
+			//{
+			//	Console.WriteLine($"Author Name = {author.FirstName} {author.LastName}");
+			//	foreach (var book in author.AuthorBooks)
+			//	{
+			//		Console.WriteLine($"\t{book.Title}");
+			//	}
+			//	Console.WriteLine("====================================================");
+			//}
+
+
+			//var authors = dbContext.Authors.Select(a => new
+			//                                       	{
+			//                                       		Name = a.FirstName + " " + a.LastName,
+			//                                       		Books = a.AuthorBooks.Select(b => b.Title)
+			//                                       	}).ToList();
+
+			//foreach (var a in authors)
+			//{
+			//	Console.WriteLine($"Author Name = {a.Name}");
+			//	foreach (var title in a.Books)
+			//		Console.WriteLine($"\t{title}");
+			//	Console.WriteLine("====================================================");
+			//}
+
+			#endregion
+
+			#region Member with id 1 Want To Borrow The Book With Id 2 And He Will Return it After 5 Days 
+
+			//int MemberId = 1;
+			//int BookId = 2;
+			//int BorrowDays = 5;
+
+			//bool Done = LoanManagement.BorrowingBook(MemberId, BookId, BorrowDays, dbContext);
+
+			//if (Done)
+			//{
+			//	Console.WriteLine($"Member {MemberId} successfully borrowed Book {BookId} for {BorrowDays} days");
+			//}
+			//else
+			//{
+			//	Console.WriteLine("Borrowing failed. Check member status, book availability");
+			//}
+
+			#endregion
+
+			#region After 10 Days Member with id 1 Returned The Book
+			//int MemberId = 10;
+			//int BookId = 2;
+
+			//bool Returned = LoanManagement.ReturnBook(MemberId, BookId, dbContext);
+			//if (Returned)
+			//{
+			//	Console.WriteLine("Book returned successfully.");
+			//}
+			//else
+			//{
+			//	Console.WriteLine("Return failed. Maybe already returned or no matching loan");
+			//}
+
+			#endregion
+
+			#region Retrieve all members who currently have active loans 
+
+			//var membersWithActiveLoans = dbContext.Members
+			// .Where(m => dbContext.MemberLoans.Any(ml => ml.MemberId == m.Id && ml.ReturnDate == null))
+			// .ToList();
+
+			//foreach (var item in membersWithActiveLoans)
+			//	Console.WriteLine(item.Name);
+
+			#endregion
+
+			#endregion
 		}
 	}
 }
